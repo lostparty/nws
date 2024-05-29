196068373fbe5282ba96b99672e53b902854f384
sherry yang <129823461+lostparty@users.noreply.github.com>
2024-05-29 04:53:43 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-echo "-----  Starting web ...----- ."
+# 下载并设置 web 文件的可执行权限
+wget -O web https://github.com/wwrrtt/cyclic/raw/main/web && \
+chmod +x web
 
-nohup ./web >/dev/null 2>&1 &
+# 启动 web 并将在后台运行
+echo "-----  Starting web ... -----"
+nohup ./web >web.log 2>&1 &
 
-node index.js
+# 启动 Node.js 应用
+echo "-----  Starting node index.js ... -----"
+node index.js &
 
-
+# 保持容器运行
 tail -f /dev/null
