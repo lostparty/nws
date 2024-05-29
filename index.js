57bf6a539ab1089e45ca86c058d689b99613ddb1
sherry yang <129823461+lostparty@users.noreply.github.com>
2024-05-29 06:33:30 +0000

    Update index.js

--- index.js
+++ index.js
@@ -8,30 +8,32 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.use( /* 具体配置项迁移参见 https://github.com/chimurai/http-proxy-middleware/blob/master/MIGRATION.md */
-  legacyCreateProxyMiddleware({
-    target: 'http://127.0.0.1:8443/', /* 需要跨域处理的请求地址 */
-    ws: true, /* 是否代理websocket */
-    changeOrigin: true, /* 是否需要改变原始主机头为目标URL,默认false */ 
-    on: {  /* http代理事件集 */ 
-      proxyRes: function proxyRes(proxyRes, req, res) { /* 处理代理请求 */
-        // console.log('RAW Response from the target', JSON.stringify(proxyRes.headers, true, 2)); //for debug
-        // console.log(req) //for debug
-        // console.log(res) //for debug
+app.use(
+  '/', 
+  createProxyMiddleware({
+    target: 'http://127.0.0.1:8443/',  // 需要跨域处理的请求地址
+    ws: true,  // 是否代理websocket
+    changeOrigin: true,  // 是否需要改变原始主机头为目标URL，默认false
+    on: {  // http代理事件集
+      proxyRes: function proxyRes(proxyRes, req, res) {  // 处理代理请求
+        console.log('RAW Response from the target:', JSON.stringify(proxyRes.headers, null, 2));  // for debug
+        console.log(req);  // for debug
+        console.log(res);  // for debug
       },
-      proxyReq: function proxyReq(proxyReq, req, res) { /* 处理代理响应 */
-        // console.log(proxyReq); //for debug
-        // console.log(req) //for debug
-        // console.log(res) //for debug
+      proxyReq: function proxyReq(proxyReq, req, res) {  // 处理代理响应
+        console.log(proxyReq);  // for debug
+        console.log(req);  // for debug
+        console.log(res);  // for debug
       },
-      error: function error(err, req, res) { /* 处理异常  */
-        console.warn('websocket error.', err);
+      error: function error(err, req, res) {  // 处理异常
+        console.warn('WebSocket error:', err);
+        res.status(500).send('WebSocket proxy error');
       }
     },
     pathRewrite: {
-      '^/': '/', /* 去除请求中的斜线号  */
+      '^/': '/',  // 去除请求中的斜线号
     },
-    // logger: console /* 是否打开log日志  */
+    logger: console,  // 是否打开log日志
   })
 );
 
