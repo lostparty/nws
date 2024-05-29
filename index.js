b23bd16df9940eb72568054035dd58adc9b9389d
sherry yang <129823461+lostparty@users.noreply.github.com>
2024-05-29 05:49:25 +0000

    Update index.js

--- index.js
+++ index.js
@@ -3,33 +3,35 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 const PORT = 3000;
-const FORWARD_PORT = 8080;
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.use(
-  '/',
-  createProxyMiddleware({
-    target: `http://127.0.0.1:${FORWARD_PORT}/`,
-    ws: true,
-    changeOrigin: true,
-    on: {
-      proxyRes: function (proxyRes, req, res) {
-        // 响应处理逻辑
+app.use( /* 具体配置项迁移参见 https://github.com/chimurai/http-proxy-middleware/blob/master/MIGRATION.md */
+  legacyCreateProxyMiddleware({
+    target: 'http://127.0.0.1:8080/', /* 需要跨域处理的请求地址 */
+    ws: true, /* 是否代理websocket */
+    changeOrigin: true, /* 是否需要改变原始主机头为目标URL,默认false */ 
+    on: {  /* http代理事件集 */ 
+      proxyRes: function proxyRes(proxyRes, req, res) { /* 处理代理请求 */
+        // console.log('RAW Response from the target', JSON.stringify(proxyRes.headers, true, 2)); //for debug
+        // console.log(req) //for debug
+        // console.log(res) //for debug
       },
-      proxyReq: function (proxyReq, req, res) {
-        // 请求处理逻辑
+      proxyReq: function proxyReq(proxyReq, req, res) { /* 处理代理响应 */
+        // console.log(proxyReq); //for debug
+        // console.log(req) //for debug
+        // console.log(res) //for debug
       },
-      error: function (err, req, res) {
-        console.warn('WebSocket error.', err);
+      error: function error(err, req, res) { /* 处理异常  */
+        console.warn('websocket error.', err);
       }
     },
     pathRewrite: {
-      '^/': '/',
+      '^/': '/', /* 去除请求中的斜线号  */
     },
-    logger: console,
+    // logger: console /* 是否打开log日志  */
   })
 );
 
