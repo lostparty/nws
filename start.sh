290f0aeea07d04ee117fb30c18cef679dc86ce07
sherry yang <129823461+lostparty@users.noreply.github.com>
2024-05-29 06:09:24 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ wget -O temp.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray
 
 # 启动 web 并将在后台运行
 echo "----- Starting web ... -----"
-nohup ./web run -config ./config.json >web.log 2>&1 &
+nohup ./web run -config /app/config.json >web.log 2>&1 &
 
 # 确保 web 服务启动后开始运行 node index.js
 sleep 2  # 等待 web 服务真正启动。
