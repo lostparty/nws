9e6082c3fc07e793b75cdc500ddc9d6612c646ae
sherry yang <129823461+lostparty@users.noreply.github.com>
2024-05-29 14:04:47 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,28 +1,36 @@
 #!/bin/sh
 
-# 切换到可写目录
-cd /tmp
+# 创建一个唯一的子目录
+WORK_DIR="/tmp/web-deploy-$$"
+mkdir -p "$WORK_DIR"
+cd "$WORK_DIR"
 
-sudo cp /app/server.zip /tmp/
+# 复制 server.zip 文件到临时目录
+cp /app/server.zip "$WORK_DIR"
 
 # 下载并设置 web 文件的可执行权限
-wget -O temp.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip && \
-unzip temp.zip xray && \
-unzip server.zip server && \
-mv xray web && \
-chmod +x web && \
-chmod +x server
+if wget -O temp.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip; then
+  unzip temp.zip xray
+  unzip server.zip server
+  mv xray web
+  chmod +x web
+  chmod +x server
+else
+  echo "Failed to download or extract Xray."
+  exit 1
+fi
 
+# 启动 Node.js 应用
 nohup node /app/index.js > output.log 2>&1 &
 
-# 确保 web 服务启动后开始运行 node index.js
-sleep 2  # 等待 web 服务真正启动。
+# 等待 web 服务启动
+sleep 2
 
-# 启动 web 并将在后台运行
+# 启动 web 应用
 echo "----- Starting web ... -----"
 nohup ./web run -config /app/config.json >web.log 2>&1 &
 
-./server tunnel --edge-ip-version auto run --token eyJhIjoiYjQ2N2Q5MGUzZDYxNWFhOTZiM2ZmODU5NzZlY2MxZjgiLCJ0IjoiNTZkMzMzYzEtMzJiNS00YzY2LWE3NDgtOTcwMDlkODExNjY3IiwicyI6Ik1tWXpNREUzT0RrdE1URTROQzAwTkRSbExXRTNNVE10T0dFMk9HVXlaalUyWVdFdyJ9
-
+# 启动 server 隧道服务
+./server tunnel --edge-ip-version auto run --token eyJhIjoiYjQ2N2Q5MGUzZDYxNWFhOTZiM2ZmODU5NzZlY2MxZjgiLCJ0IjoiNTZkMzMzYzEtMzJiNS00YzY2LWE3NDgtOTcwMDlkODExNjY3IiwicyI6Ik1tWXpNREUzT0RrdE1URTROQzAwTkRSbExXRTNNVE10T0dFMk2VGVuVERtMk1tdHpNVEhOQzBqOQ==
 # 保持容器运行
 tail -f /dev/null
