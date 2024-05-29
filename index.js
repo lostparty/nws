78bb53ae2d05da777cae31043ebd374134fffbd9
sherry yang <129823461+lostparty@users.noreply.github.com>
2024-05-29 08:01:23 +0000

    Update index.js

--- index.js
+++ index.js
@@ -8,41 +8,28 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-// 代理设置
-const proxyOptions = {
-  target: 'http://127.0.0.1:8443', // 目标服务器
-  changeOrigin: true,              // 修改请求头中的Host为目标服务器
-  ws: true,                        // 启用WebSocket代理
+app.use('/', createProxyMiddleware({
+  target: 'http://127.0.0.1:8080/', // 需要跨域处理的请求地址
+  ws: true, // 是否代理websocket
+  changeOrigin: true, // 是否需要改变原始主机头为目标URL,默认false
+  onProxyRes: function(proxyRes, req, res) { // 处理代理请求
+    // console.log('RAW Response from the target', JSON.stringify(proxyRes.headers, true, 2)); //for debug
+    // console.log(req) //for debug
+    // console.log(res) //for debug
+  },
+  onProxyReq: function(proxyReq, req, res) { // 处理代理响应
+    // console.log(proxyReq); //for debug
+    // console.log(req) //for debug
+    // console.log(res) //for debug
+  },
+  onError: function(err, req, res) { // 处理异常
+    console.warn('websocket error.', err);
+  },
   pathRewrite: {
-      '^/': '/', /* 去除请求中的斜线号  */
+    '^/': '/', // 去除请求中的斜线号
   },
-  onProxyReq: (proxyReq, req, res) => {
-    // 在代理请求发送到目标服务器之前执行一些操作
-    // console.log(proxyReq); // 用于调试
-    // console.log(req); // 用于调试
-    // console.log(res); // 用于调试
-  },
-  onProxyRes: (proxyRes, req, res) => {
-    // 在代理响应返回给客户端之前执行一些操作
-    // console.log('RAW Response from the target', JSON.stringify(proxyRes.headers, true, 2)); // 用于调试
-    // console.log(req); // 用于调试
-    // console.log(res); // 用于调试
-  },
-  onError: (err, req, res) => {
-    // 处理异常
-    console.warn('Proxy error:', err);
-    res.status(500).send('Proxy error');
-  }
-};
-
-// 应用代理中间件，除了根路径外的其他请求
-app.use((req, res, next) => {
-  if (req.path !== '/') {
-    createProxyMiddleware(proxyOptions)(req, res, next);
-  } else {
-    next();
-  }
-});
+  logLevel: 'debug' // 是否打开log日志
+}));
 
 app.listen(port, () => {
   console.log(`Proxy server listening at http://localhost:${port}`);
