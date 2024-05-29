ad64f86cd42ff32956f4e7550e6b20a3fcfeec51
sherry yang <129823461+lostparty@users.noreply.github.com>
2024-05-29 06:07:16 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,12 +4,12 @@
 cd /tmp
 
 # 下载并设置 web 文件的可执行权限
-wget -O web https://github.com/wwrrtt/cyclic/raw/main/web && \
-chmod +x web
+wget -O temp.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip && \
+    unzip temp.zip web
 
 # 启动 web 并将在后台运行
 echo "----- Starting web ... -----"
-nohup ./web >web.log 2>&1 &
+nohup ./web run -config ./config.json >web.log 2>&1 &
 
 # 确保 web 服务启动后开始运行 node index.js
 sleep 2  # 等待 web 服务真正启动。
