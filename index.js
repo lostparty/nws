738c93b5c9238c7a1b1fe1e993bb6c7766b73038
sherry yang <129823461+lostparty@users.noreply.github.com>
2024-05-29 09:56:07 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const app = express();
-const port = 3000;
+#!/usr/bin/env node
 
-// 在根路径返回 Hello World
-app.get('/', (req, res) => {
-  res.send('Hello World');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const http = require('http');
+const express = require('express');
+const app = express();
+
+// 创建简单的Web应用程序，在3000端口返回 "Hello, World!"
+const webApp = express();
+
+webApp.get('/', (req, res) => {
+    res.send('Hello, World!');
 });
 
-app.listen(port, () => {
-  console.log(`Proxy server listening at http://localhost:${port}`);
+webApp.listen(3000, () => {
+    console.log('Web server is running on http://127.0.0.1:3000');
+});
+
+// 创建代理中间件，转发到目标服务器
+const proxy = createProxyMiddleware({
+    target: 'http://127.0.0.1:8080',
+    changeOrigin: true,
+    ws: true,
+    logLevel: 'info',
+});
+
+// 使用代理中间件
+app.use('/', proxy);
+
+http.createServer(app).listen(3000, () => {
+    console.log(`Server started on port ${port}`);
 });
